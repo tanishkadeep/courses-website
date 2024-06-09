a4d4febd97e973ec93de615905be827c45ed1f0d
Tanishka <tanishkadeep09@gmail.com>
2024-06-09 13:05:04 +0530

    homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import {
   getCoursesStatus,
   fetchCourses,
 } from "../features/courses/coursesSlice";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   const courses = useSelector(selectAllCourses);
@@ -27,7 +28,29 @@ export const Home = () => {
 
   let content;
 
-  content = filteredCourses.map((course) => JSON.stringify(course));
+  content = filteredCourses.map((course) => (
+    <div className="p-4 mx-4 my-8 md:mx-10 border rounded-lg shadow-md">
+      <Link
+        to={`/courses/${course.id}`}
+        className="flex flex-col md:flex-row items-center"
+      >
+        <div>
+          <img
+            src={course.thumbnail}
+            alt={course.name}
+            className="min-w-40 md:max-w-80"
+          />
+        </div>
+        <div className="md:pl-10 w-full pt-5 md:pt-0">
+          <div className="text-3xl font-extrabold">{course.name}</div>
+          <div className="py-2 italic text-gray-700 font-semibold">
+            {course.instructor}
+          </div>
+          <div className="text-sm ">{course.description}</div>
+        </div>
+      </Link>
+    </div>
+  ));
 
   return (
     <div className=" mx-5 lg:mx-40 my-8">
@@ -64,7 +87,7 @@ export const Home = () => {
           </div>
         </div>
       </div>
-      <div className="mt-5 border-4 border-b-gray-300 rounded-md"></div>
+      <div className="mt-5 mb-10 border-4 border-b-gray-300 rounded-md"></div>
       <div>{content}</div>
     </div>
   );

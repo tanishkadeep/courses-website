340b2aacc37f99f1bcd3c35ae0f065fcbd944244
Tanishka <tanishkadeep09@gmail.com>
2024-06-10 18:21:35 +0530

    added loader

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -10,6 +10,7 @@ import {
 } from "../features/courses/coursesSlice";
 import { ProgressBar } from "../components/ProgressBar";
 import { Link } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 
 export const Dashboard = () => {
   const courses = useSelector(selectAllCourses);
@@ -20,7 +21,7 @@ export const Dashboard = () => {
     if (courseStatus === "idle") {
       dispatch(fetchCourses());
     }
-  }, [courseStatus, courses]);
+  }, [courseStatus, dispatch]);
 
   const enrolledCourses = courses.filter(
     (course) => course.enrollmentStatus === "Enrolled"
@@ -35,42 +36,50 @@ export const Dashboard = () => {
       <Appbar />
       <div className="my-16 md:mx-24 sm:mx-16 mx-8 lg:mx-32">
         <div className="text-4xl font-extrabold mb-8">My Courses</div>
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 justify-center items-center">
-          {enrolledCourses.map((course) => (
-            <div className="border-2 border-gray-300 rounded-lg">
-              <Link to={`/courses/${course.id}`}>
-                <img
-                  src={course.thumbnail}
-                  alt={course.name}
-                  className="w-full  h-44 sm:h-56  object-cover rounded-t-lg"
-                />
-              </Link>
-              <div className="p-4">
-                <Link
-                  to={`/courses/${course.id}`}
-                  className="text-xl font-bold"
-                >
-                  {course.name}
-                </Link>
-                <div className="italic py-2">{course.instructor}</div>
-                <div className="text-sm">Due: {course.duration}</div>
-              </div>
-              <div className="px-4 flex justify-between items-center mb-6">
-                <ProgressBar progress={course.progress} />
-                <div className="pl-4 text-sm font-medium">
-                  {course.progress}%
-                </div>
-              </div>
-
-              <button
-                onClick={() => handleMarkAsCompleted(course.id)}
-                className="bg-gray-100 cursor-pointer py-2 font-semibold hover:bg-gray-200 w-full"
+        {courseStatus === "loading" ? (
+          <div className="flex justify-center items-center h-64">
+            <ClipLoader size={40} color={"#123abc"} loading={true} />
+          </div>
+        ) : (
+          <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 justify-center items-center">
+            {enrolledCourses.map((course) => (
+              <div
+                key={course.id}
+                className="border-2 border-gray-300 rounded-lg"
               >
-                Mark as completed
-              </button>
-            </div>
-          ))}
-        </div>
+                <Link to={`/courses/${course.id}`}>
+                  <img
+                    src={course.thumbnail}
+                    alt={course.name}
+                    className="w-full h-44 sm:h-56 object-cover rounded-t-lg"
+                  />
+                </Link>
+                <div className="p-4">
+                  <Link
+                    to={`/courses/${course.id}`}
+                    className="text-xl font-bold"
+                  >
+                    {course.name}
+                  </Link>
+                  <div className="italic py-2">{course.instructor}</div>
+                  <div className="text-sm">Due: {course.duration}</div>
+                </div>
+                <div className="px-4 flex justify-between items-center mb-6">
+                  <ProgressBar progress={course.progress} />
+                  <div className="pl-4 text-sm font-medium">
+                    {course.progress}%
+                  </div>
+                </div>
+                <button
+                  onClick={() => handleMarkAsCompleted(course.id)}
+                  className="bg-gray-100 cursor-pointer py-2 font-semibold hover:bg-gray-200 w-full"
+                >
+                  Mark as completed
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
